d27f292378a3f3d041db6d861681475b811f3249
Andreu729 <andresesparzag@gmail.com>
2024-06-04 12:29:44 -0400

    master change

--- xd.py
+++ xd.py
@@ -1,6 +1,13 @@
 print("Hola mundo")
 # a
+
 for i in range(0,10):
     print("ya para con química")
 
 print("DEJA DE PENSAR EN QUÍMICA")
+
+a = input("Hola vocé eres tontitotation: ")
+if a.lower() == "no":
+    print("que si imbecil")
+elif a.lower() == "si":
+    print("que vien ke lo aseptes xd")
