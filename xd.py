0091565878a92daef17749a2c46518522f7d8e8e
Andreu729 <andresesparzag@gmail.com>
2024-06-04 12:41:08 -0400

    tonto changes

--- xd.py
+++ xd.py
@@ -1,13 +1,14 @@
 print("Hola mundo")
 # a
 
+
 for i in range(0,10):
     print("ya para con química")
-
-print("DEJA DE PENSAR EN QUÍMICA")
 
 a = input("Hola vocé eres tontitotation: ")
 if a.lower() == "no":
     print("que si imbecil")
-elif a.lower() == "si":
-    print("que vien ke lo aseptes xd")
+elif a.lower() == "si" or a.lower() == "sí":
+    print("que bien que lo aceptes xd")
+else:
+    print("ah? que significa eso?")
