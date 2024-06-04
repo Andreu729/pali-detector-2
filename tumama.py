d4ac972f8ae192c7884e84d12e0641203012a0d2
Andreu729 <andresesparzag@gmail.com>
2024-06-04 11:35:25 -0400

    odd deleter

--- tumama.py
+++ tumama.py
@@ -8,3 +8,13 @@ def element_deleter(list_, pos):
             mod_list.append(e)
         i += 1
     return mod_list
+
+
+def odd_deleter(a):
+    if len(a) % 2 == 1:
+        pos = (len(a)-1)/2
+        al = list(a)
+        fl = element_deleter(al, pos)
+        return "".join(fl)
+    else:
+        return a
