0d7eba1de6bb8055cd82c1fcc7be8e02fcfb77a2
Andreu729 <andresesparzag@gmail.com>
2024-06-04 19:15:39 -0400

    loop update

--- tumama.py
+++ tumama.py
@@ -1,9 +1,13 @@
 from functions import *
 
-print("Bienvenido al pali detector!")
-print("Escribe una palabra y revisaremos si es palíndromo:")
-xd = input("Escribe una palabra: ")
-if pal_detect(xd):
-    print(xd + " es un palíndromo!")
-else:
-    print("muerete pedazo de basura")
+while True:
+    print("Bienvenido al pali detector!")
+    print("Escribe una palabra y revisaremos si es palíndromo")
+    print("Presiona [1] y enter para salir")
+    xd = input("Escribe una palabra: ")
+    if xd == 1:
+        pass
+    elif pal_detect(xd):
+        print(str(xd) + " es un palíndromo!")
+    else:
+        print("muerete pedazo de basura")
