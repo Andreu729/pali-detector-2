f399f311c24aef79e2e927d919922e2f7f740b2d
Andreu729 <andresesparzag@gmail.com>
2024-06-04 12:19:12 -0400

    removing pycache

--- tumama.py
+++ tumama.py
@@ -1,20 +1,9 @@
-# xax  - xx
+from functions import *
 
-def element_deleter(list_, pos):
-    mod_list = []
-    i = 0
-    for e in list_:
-        if not (i == pos):
-            mod_list.append(e)
-        i += 1
-    return mod_list
-
-
-def odd_deleter(a):
-    if len(a) % 2 == 1:
-        pos = (len(a)-1)/2
-        al = list(a)
-        fl = element_deleter(al, pos)
-        return "".join(fl)
-    else:
-        return a
+print("Bienvenido al pali detector!")
+print("Escribe una palabra y revisaremos si es palíndromo:")
+xd = input("Escribe una palabra: ")
+if pal_detect(xd):
+    print(xd + " es un palíndromo!")
+else:
+    print("muerete pedazo de basura")
