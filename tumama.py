469dc404cb8283e9df9de4f263249a61d4ed5375
Andreu729 <andresesparzag@gmail.com>
2024-06-04 11:32:08 -0400

    element deleter

--- tumama.py
+++ tumama.py
@@ -1,1 +1,10 @@
+# xax  - xx
 
+def element_deleter(list_, pos):
+    mod_list = []
+    i = 0
+    for e in list_:
+        if not (i == pos):
+            mod_list.append(e)
+        i += 1
+    return mod_list
