7d16442bd164a7402c9798723c606ee177e6d4f6
Andreu729 <andresesparzag@gmail.com>
2024-06-07 19:03:44 -0400

    vocab change 2

--- xd.py
+++ xd.py
@@ -4,11 +4,3 @@ print("Hola mundo")
 
 for i in range(0, 10):
     print("ya para con química")
-
-a = input("Hola vocé eres tontitotation: ")
-if a.lower() == "no":
-    print("que si imbecil")
-elif a.lower() == "si" or a.lower() == "sí":
-    print("que bien que lo aceptes xd")
-else:
-    print("ah? que significa eso?")
